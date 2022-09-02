ed1a40e242e29c8486ae7a7cfd091e614cee2c90
carlosCR123 <carlosalvarado310.ca@gmail.com>
2022-09-02 14:10:35 -0600

    Initial project

---
+++ src/main/java/com/project/acentocaramelo/AcentoCarameloApplication.java
@@ -0,0 +1,13 @@
+package com.project.acentocaramelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcentoCarameloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcentoCarameloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/acentocaramelo/AcentoCarameloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.acentocaramelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcentoCarameloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
